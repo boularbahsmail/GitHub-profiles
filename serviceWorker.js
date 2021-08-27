4ce907c6d8003a41d33af011667d4685441edcdc
boularbahsmail <qiranorane55@gmail.com>
2021-08-27 12:03:14 +0100

    Turn it into PWA

---
+++ serviceWorker.js
@@ -0,0 +1,32 @@
+const cacheName = 'cache-v1';
+const resourcesToPrecache = [
+    '/',
+    'index.html',
+    'assets/css/style.css',
+    'assets/js/script.css',
+    'assets/images/logo.png'
+];
+
+// On install - the application shell cached
+// Precaching resources
+self.addEventListener('install', event => {
+    console.log('Service worker install event!');
+    event.waitUntil(
+        caches.open('cache-v1')
+            .then(cache => {
+                // Static files the make up the applicaiton shell are cached
+                return cache.addAll(resourcesToPrecache);
+
+            })
+    );
+});
+
+// Responding with only cached resources
+self.addEventListener('fetch', event => {
+    event.respondWith(caches.match(event.request)
+    .then(cacheResponse => {
+        // Return it if there is a response, or else fetch again
+        return cacheResponse || fetch(event.request);
+    })
+    );
+})
